5769b6ab34442d34733a673857d378f3d6893639
Waldirio M Pinheiro <waldirio@gmail.com>
2019-11-06 09:42:24 -0800

    parse updated (#41)
    Parse updated in order to show all the commands executed via script.

--- internals_help/parse.sh
+++ internals_help/parse.sh
@@ -2,10 +2,12 @@
 
 >executed_commands.txt
 cat ../sos_analyze.sh \
-		| grep -E '(log_tee "## |log_cmd )' \
+		| grep -E '(log_tee "## |log_cmd |cmd=)' \
 		| cut -d " " -f4- \
 		| while read line; do echo $line; done >>executed_commands.txt
 
 sed -i 's/^"//g' executed_commands.txt
 sed -i 's/"$//g' executed_commands.txt
 sed -i 's/^##/\n##/g' executed_commands.txt
+sed -i 's/^$cmd$//g' executed_commands.txt
+sed -i 's/^cmd=".*/\0"/g' executed_commands.txt
