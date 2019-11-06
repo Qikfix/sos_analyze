ab84989cc77144c7ced82e2451576e3cbd3c8433
Waldirio M Pinheiro <waldirio@gmail.com>
2019-11-06 09:04:37 -0800

    new sosreport supportability

--- sos_analyze.sh
+++ sos_analyze.sh
@@ -21,9 +21,18 @@ main()
     exit 1
   fi
 
+  if [ -d $base_dir/sos_commands/foreman/foreman-debug ]; then
+    base_foreman="/sos_commands/foreman/foreman-debug/"
+    sos_version="old"
+  else
+    sos_version="new"
+    base_foreman="/"
+  fi
+
   echo "The sosreport is: $base_dir"												| tee -a $FOREMAN_REPORT
 
-  report $base_dir $sub_dir
+  #report $base_dir $sub_dir $base_foreman $sos_version
+  report $base_dir $base_foreman $sos_version
 }
 
 log_tee()
@@ -46,9 +55,13 @@ report()
 {
 
   base_dir=$1
-  sub_dir=$2
+  # sub_dir=$2
+  # base_foreman=$base_dir/$3
+  # sos_version=$4
+  base_foreman=$base_dir/$2
+  sos_version=$3
 
-  base_foreman="$1/sos_commands/foreman/foreman-debug/"
+  #base_foreman="$1/sos_commands/foreman/foreman-debug/"
 
   log_tee "### Welcome to Report ###"
   log_tee "### CEE/SysMGMT ###"
@@ -176,31 +189,60 @@ report()
   log
 
   log "// Error on the upgrade file"
-  log "grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log | wc -l"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log | wc -l"
+  else
+    cmd="grep \"^\[ERROR\" $base_dir/var/log/foreman-installer/satellite.log | wc -l"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log	| wc -l"
+  log_cmd "$cmd"
   log "---"
   log
 
+
   log "// Error on the upgrade file (full info)"
-  log "grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+  else
+    cmd="grep \"^\[ERROR\" $base_dir/var/log/foreman-installer/satellite.log"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "grep \"^\[ERROR\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+  log_cmd "$cmd"
   log "---"
   log
 
   log "// Upgrade Completed? (6.4 or greater)"
-  log "grep \"Upgrade completed\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log | wc -l"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="grep \"Upgrade completed\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log | wc -l"
+  else
+    cmd="grep \"Upgrade completed\" $base_dir/var/log/foreman-installer/satellite.log | wc -l"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "grep \"Upgrade completed\" $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log | wc -l"
+  log_cmd "$cmd"
   log "---"
   log
 
 
   log "// last 20 lines from upgrade log"
-  log "tail -20 $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="tail -20 $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+  else
+    cmd="tail -20 $base_dir/var/log/foreman-installer/satellite.log"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "tail -20 $base_dir/sos_commands/foreman/foreman-debug/var/log/foreman-installer/satellite.log"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -215,12 +257,12 @@ report()
   log "---"
   log
 
-  log "// disk space output"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/disk_space_output"
-  log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/disk_space_output"
-  log "---"
-  log
+#  log "// disk space output"
+#  log "cat $base_dir/sos_commands/foreman/foreman-debug/disk_space_output"
+#  log "---"
+#  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/disk_space_output"
+#  log "---"
+#  log
 
   log "// no space left on device"
   log "grep -h -r \"No space left on device\" $base_dir/* 2>/dev/null"
@@ -249,15 +291,15 @@ report()
   log
 
   log "// users memory consumers"
-  log "cat \$base_foreman/ps-awfux | sort -nr | awk '{print \$1, \$6}' | grep -v ^USER | grep -v ^COMMAND | grep -v \"^ $\" | awk  '{a[\$1] += \$2} END{for (i in a) print i, a[i]}' | sort -nrk2"
+  log "cat $base_dir/ps | sort -nr | awk '{print \$1, \$6}' | grep -v ^USER | grep -v ^COMMAND | grep -v \"^ $\" | awk  '{a[\$1] += \$2} END{for (i in a) print i, a[i]}' | sort -nrk2"
   log "and"
-  log "memory_usage=\$(cat $base_foreman/ps-awfux | sort -nr | awk '{print \$6}' | grep -v ^RSS | grep -v ^$ | paste -s -d+ | bc)"
+  log "memory_usage=\$(cat $base_dir/ps | sort -nr | awk '{print \$6}' | grep -v ^RSS | grep -v ^$ | paste -s -d+ | bc)"
   log "and"
   log "memory_usage_gb=\$(echo \"scale=2;$memory_usage/1024/1024\" | bc)"
   log "---"
-  log_cmd "cat $base_foreman/ps-awfux | sort -nr | awk '{print \$1, \$6}' | grep -v ^USER | grep -v ^COMMAND | grep -v \"^ $\" | awk  '{a[\$1] += \$2} END{for (i in a) print i, a[i]}' | sort -nrk2"
+  log_cmd "cat $base_dir/ps | sort -nr | awk '{print \$1, \$6}' | grep -v ^USER | grep -v ^COMMAND | grep -v \"^ $\" | awk  '{a[\$1] += \$2} END{for (i in a) print i, a[i]}' | sort -nrk2"
   log
-  memory_usage=$(cat $base_foreman/ps-awfux | sort -nr | awk '{print $6}' | grep -v ^RSS | grep -v ^$ | paste -s -d+ | bc)
+  memory_usage=$(cat $base_dir/ps | sort -nr | awk '{print $6}' | grep -v ^RSS | grep -v ^$ | paste -s -d+ | bc)
   memory_usage_gb=$(echo "scale=2;$memory_usage/1024/1024" | bc)
   log "Total Memory Consumed in KiB: $memory_usage"
   log "Total Memory Consumed in GiB: $memory_usage_gb"
@@ -265,23 +307,23 @@ report()
   log
 
   log "// Postgres idle process (candlepin)"
-  log "cat \$base_foreman/ps-awfux | grep ^postgres | grep idle$ | grep \"candlepin candlepin\" | wc -l"
+  log "cat $base_dir/ps | grep ^postgres | grep idle$ | grep \"candlepin candlepin\" | wc -l"
   log "---"
-  log_cmd "cat $base_foreman/ps-awfux | grep ^postgres | grep idle$ | grep \"candlepin candlepin\" | wc -l"
+  log_cmd "cat $base_dir/ps | grep ^postgres | grep idle$ | grep \"candlepin candlepin\" | wc -l"
   log "---"
   log
 
   log "// Postgres idle process (foreman)"
-  log "cat \$base_foreman/ps-awfux | grep ^postgres | grep idle$ | grep \"foreman foreman\" | wc -l"
+  log "cat $base_dir/ps | grep ^postgres | grep idle$ | grep \"foreman foreman\" | wc -l"
   log "---"
-  log_cmd "cat $base_foreman/ps-awfux | grep ^postgres | grep idle$ | grep \"foreman foreman\" | wc -l"
+  log_cmd "cat $base_dir/ps | grep ^postgres | grep idle$ | grep \"foreman foreman\" | wc -l"
   log "---"
   log
 
   log "// Postgres idle process (everything)"
-  log "cat \$base_foreman/ps-awfux | grep ^postgres | grep idle$ | wc -l"
+  log "cat $base_dir/ps | grep ^postgres | grep idle$ | wc -l"
   log "---"
-  log_cmd "cat $base_foreman/ps-awfux | grep ^postgres | grep idle$ | wc -l"
+  log_cmd "cat $base_dir/ps | grep ^postgres | grep idle$ | wc -l"
   log "---"
   log
 
@@ -323,17 +365,34 @@ report()
   log_tee "## Foreman Tasks"
   log
 
+  
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | wc -l"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/foreman_tasks_tasks | wc -l"
+  fi
+
   log "// total # of foreman tasks"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | wc -l"
+  #log "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | wc -l"
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | wc -l"
+  #log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | wc -l"
+  log_cmd "$cmd"
   log "---"
   log
 
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | cut -d, -f3 | grep Actions | sort | uniq -c | sort -nr"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/foreman_tasks_tasks | sed '1,3d' | cut -d\| -f3 | grep Actions | sort | uniq -c | sort -nr"
+  fi
+
+
   log "// Tasks TOP"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | cut -d, -f3 | grep Actions | sort | uniq -c | sort -nr"
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/foreman_tasks_tasks.csv | cut -d, -f3 | grep Actions | sort | uniq -c | sort -nr"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -342,9 +401,16 @@ report()
   log
 
   log "// hammer ping output"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/hammer-ping"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/hammer-ping"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/hammer_ping"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/hammer-ping"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -353,16 +419,23 @@ report()
   log
 
   log "// katello-service status output"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/katello_service_status"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/katello_service_status"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/foreman-maintain_service_status"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/katello_service_status"
+  log_cmd "$cmd"
   log "---"
   log
 
 
   log_tee "## MongoDB Storage"
   log
-
+  # FIX
   log "// mongodb storage consumption"
   log "cat $base_dir/sos_commands/foreman/foreman-debug/mongodb_disk_space"
   log "---"
@@ -375,9 +448,16 @@ report()
   log
 
   log "// postgres storage consumption"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/postgres_disk_space"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/postgres_disk_space"
+  else
+    cmd="cat $base_dir/sos_commands/postgresql/du_-sh_.var.lib.pgsql"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/postgres_disk_space"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -386,16 +466,30 @@ report()
   log
 
   log "// current passenger status"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_pool"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_pool"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/passenger-status_--show_pool"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_pool"
+  log_cmd "$cmd"
   log "---"
   log
 
   log "// URI requests"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_requests | grep uri | sort -k3 | uniq -c"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_requests | grep uri | sort -k3 | uniq -c"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/passenger-status_--show_requests | grep uri | sort -k3 | uniq -c"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/passenger_status_requests | grep uri | sort -k3 | uniq -c"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -404,9 +498,9 @@ report()
   log
 
   log "// dynflow running"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/ps-awfux  | grep dynflow_executor\$"
+  log "cat $base_dir/ps | grep dynflow_executor\$"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/ps-awfux  | grep dynflow_executor$"
+  log_cmd "cat $base_dir/ps | grep dynflow_executor$"
   log "---"
   log
 
@@ -416,24 +510,45 @@ report()
   log
 
   log "// katello_event_queue (foreman-tasks / dynflow is running?)"
-  log "grep -E '(  queue|  ===|katello_event_queue)' $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="grep -E '(  queue|  ===|katello_event_queue)' $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q"
+  else
+    cmd="grep -E '(  queue|  ===|katello_event_queue)' $base_dir/sos_commands/qpid/qpid-stat_-q"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "grep -E '(  queue|  ===|katello_event_queue)' $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q"
+  log_cmd "$cmd"
   log "---"
   log
 
 
   log "// total number of pulp agents"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | wc -l"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | wc -l"
+  else
+    cmd="cat $base_dir/sos_commands/qpid/qpid-stat_-q | grep pulp.agent | wc -l"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | wc -l"
+  log_cmd "$cmd"
   log "---"
   log
 
   log "// total number of (active) pulp agents"
-  log "cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | grep \" 1.*1\$\" | wc -l"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | grep \" 1.*1\$\" | wc -l"
+  else
+    cmd="cat $base_dir/sos_commands/qpid/qpid-stat_-q | grep pulp.agent | grep \" 1.*1\$\" | wc -l"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_dir/sos_commands/foreman/foreman-debug/qpid-stat-q | grep pulp.agent | grep \" 1.*1\$\" | wc -l"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -579,9 +694,16 @@ report()
   log
 
   log "// latest 30 hypervisors tasks"
-  log "cat $base_foreman/foreman_tasks_tasks.csv | grep Hypervisors | sed -e 's/,/ /g' | sort -rk4 | head -n 30"
+
+  if [ "$sos_version" == "old" ];then
+    cmd="cat $base_foreman/foreman_tasks_tasks.csv | grep Hypervisors | sed -e 's/,/ /g' | sort -rk4 | head -n 30"
+  else
+    cmd="cat $base_dir/sos_commands/foreman/foreman_tasks_tasks | grep Hypervisors | sed -e 's/,/ /g' | sort -rk6 | head -n 30"
+  fi
+
+  log "$cmd"
   log "---"
-  log_cmd "cat $base_foreman/foreman_tasks_tasks.csv | grep Hypervisors | sed -e 's/,/ /g' | sort -rk4 | head -n 30"
+  log_cmd "$cmd"
   log "---"
   log
 
@@ -590,9 +712,9 @@ report()
   log
 
   log "// Memory (Xms and Xmx)"
-  log "grep tomcat $base_foreman/ps-awfux"
+  log "grep tomcat $base_dir/ps"
   log "---"
-  log_cmd "grep tomcat $base_foreman/ps-awfux"
+  log_cmd "grep tomcat $base_dir/ps"
   log "---"
   log
 
