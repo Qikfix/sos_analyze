77997872920a8f3eeb4acaf047819d5cb8c1503d
Waldirio M Pinheiro <waldirio@gmail.com>
2024-08-02 11:52:40 -0700

    [FIX] fixing some known issues
    * fixing some known issues
    * fixing the parse output

--- internals_help/parse.sh
+++ internals_help/parse.sh
@@ -6,8 +6,11 @@ cat ../sos_analyze.sh \
 		| cut -d " " -f4- \
 		| while read line; do echo $line; done >>executed_commands.txt
 
-sed -i 's/^"//g' executed_commands.txt
-sed -i 's/"$//g' executed_commands.txt
-sed -i 's/^##/\n##/g' executed_commands.txt
-sed -i 's/^$cmd$//g' executed_commands.txt
-sed -i 's/^cmd=".*/\0"/g' executed_commands.txt
+# Adding the "" because the sed on MAC is working as BSD system
+# which requires a parameter after the "-i"
+
+sed -i "" 's/^"//g' executed_commands.txt
+sed -i "" 's/"$//g' executed_commands.txt
+sed -i "" 's/^##/\n##/g' executed_commands.txt
+sed -i "" 's/^$cmd$//g' executed_commands.txt
+sed -i "" 's/^cmd=".*/\0"/g' executed_commands.txt
